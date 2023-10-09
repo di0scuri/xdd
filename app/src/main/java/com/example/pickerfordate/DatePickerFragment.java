298a088b75c5bad309682ea0b59c590acf3a7e9a
Lord Orendain <118495617+di0scuri@users.noreply.github.com>
2023-10-09 19:53:27 +0800

    10/9/2023 7:53PM

---
+++ app/src/main/java/com/example/pickerfordate/DatePickerFragment.java
@@ -0,0 +1,52 @@
+package com.example.pickerfordate;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.widget.DatePicker;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+    // Rename and change types of parameters
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private String mParam1;
+    private String mParam2;
+
+    public DatePickerFragment() {
+        // Remove the empty public constructor
+    }
+
+    public static DatePickerFragment newInstance(String param1, String param2) {
+        DatePickerFragment fragment = new DatePickerFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(), this, year, month, dayOfMonth);
+    }
+
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        MainActivity activity = (MainActivity) getActivity();
+        activity.processDatePickerResult(year, month, day);
+    }
+
+}
